6e9957b5268d0f554e1396aa10d76b92e53fd617
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-10-24 12:14:07 -0300

    feat: create class Pessoa

--- src/Programa/Pessoa.java
+++ src/Programa/Pessoa.java
@@ -1,2 +1,44 @@
-package Programa;public class Pessoa {
+package Programa;
+
+public class Pessoa {
+    private static int counter = 1;
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        counter++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String setNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String setCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String setEmail() {
+        return email;
+    }
+
+    public String toString() {
+        return "\nNome: " + this.getNome() + "\nCPF: " + this.getCpf() + "\nEmail: " + this.getEmail();
+    }
 }
